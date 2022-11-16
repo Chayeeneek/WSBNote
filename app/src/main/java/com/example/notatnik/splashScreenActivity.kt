0e980f658fba56b25d2eee1bbc3bd259aa2da017
GoleM20 <87028524+GoleM20@users.noreply.github.com>
2022-11-16 21:44:42 +0100

    Maciej commit - splash screen add

---
+++ app/src/main/java/com/example/notatnik/splashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.notatnik
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashScreenActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 5000 // 5 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            // close this activity
+            finish()
+        }, splashTimeOut)
+    }
+}
