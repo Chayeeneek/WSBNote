451a98b442a48314dae74cc9e8ce40e7b82c7275
Chayeeneek <chayeeneek@gmail.com>
2022-11-17 22:08:29 +0100

    Michał commit

--- app/src/main/java/com/example/notatnik/splashScreenActivity.kt
+++ app/src/main/java/com/example/notatnik/splashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.notatnik
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -8,11 +9,14 @@ import android.os.Looper
 
 class splashScreenActivity : AppCompatActivity() {
 
+
     private val splashTimeOut: Long = 5000 // 5 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
         Handler(Looper.getMainLooper()).postDelayed({
             // This method will be executed once the timer is over
             // Start your app main activity
